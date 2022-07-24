acdb26215d5e36854a170bc7b79a188b48245d13
Luciano Garbarino <lucigar01@gmail.com>
2022-07-24 01:03:04 -0300

    cors

--- backend/engine/main.py
+++ backend/engine/main.py
@@ -1,5 +1,6 @@
 import logging
 from reader import FundsScrapper
+import Levenshtein as lv
 
 # Logging config
 logging.basicConfig(
@@ -12,6 +13,10 @@ logging.basicConfig(
 
 # Configs
 FUNDS_FILE = "funds.csv"
+TICKERS_NAMES_FILE = "ticker_names"
+
+MIN_CATCH = 0.62 # lv_ratio_matcher() will start catching names from MIN_CATCH
+MAX_CATCH = 0.8  # lv_ratio_matcher() will take the name without continuing
 # --------------------------------
 
 def load_parameters() -> list:
@@ -28,7 +33,33 @@ def load_parameters() -> list:
         print("[ERROR] Load parameters")
         logging.error("[ERROR] Load parameters: " + str(e))
         return []
+
+def lv_ratio_matcher(name: str, all_tickers: list) -> str:
+    return name + " c"
+
+def symbols_update(funds_data: dict) -> None:
+    tickers_not_found = []
+    tickers_not_found_index = []
+    with open(FUNDS_FILE) as f:
+            all_tickers: list = []
+            for line in f:
+                all_tickers.append(line.strip().split(","))
+
+    for fund in funds_data.keys():
+        for i in range(len(funds_data[fund]["tickers"]["names"])):
+            match: str = lv_ratio_matcher(funds_data[fund]["tickers"]["names"][i],all_tickers)
+            if match != "":
+                funds_data[fund]["tickers"]["names"][i] = match
+            else:
+                tickers_not_found_index.append(i)
         
+        for t in tickers_not_found_index:
+            funds_data[fund]["tickers"]["qty_ars"].pop(t)
+            tickers_not_found.append(funds_data[fund]["tickers"]["names"].pop(t))
+
+    
+
+
 def price_qty_update(funds_data: dict) -> None:
 
     for fund in funds_data.values():
@@ -44,8 +75,11 @@ def main() -> None:
     for i in funds_data["error"]:
         logging.error(i)
     funds_data = funds_data["data"]
+    symbols_update(funds_data)
     price_qty_update(funds_data)
-    return funds_data
+    print(funds_data)
 
 
-print(main())
+main()
+
+#print(lv.ratio("GRUPO SUPERVIELLE CB","BANCO SUPERVIELLE"))
