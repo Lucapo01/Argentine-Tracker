a83671a6ef8da68271ac17a488ed8a132963ea8e
lucasjang01 <lucasjang01@gmail.com>
2022-09-26 21:42:50 -0300

    detail ascending and descending funds order

--- frontend/src/components/Funds.js
+++ frontend/src/components/Funds.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import './Funds.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown} from '@fortawesome/free-solid-svg-icons'
 
 const Funds = () => {
     const { id } = useParams()
     const { date } = useParams()
     const [totalFunds, setTotalFunds] = useState({})
+    const [fundsList, setFundsList] = useState({})
+    const [descendingOrder, setDescendingOrder] = useState(true)
 
     useEffect(() => {
         const fetchFunds = async () => {
             const res = await fetch(`http://localhost:8000/point/${id}/${date}`)
             const data = await res.json()
             setTotalFunds(data)
+            const fundsListFromServer = Object.keys(data.funds).map((key) => {return [key, data.funds[key]]})
+            fundsListFromServer.sort((first, second) => {
+                return second[1] - first[1];
+            })
+            setFundsList(fundsListFromServer)
         }
 
         fetchFunds()
     }, [id, date])
+
+    const changeFundsListOrder = () => {
+        setDescendingOrder(!descendingOrder)
+        const desendingOrderAux = !descendingOrder
+        if (desendingOrderAux) {
+            fundsList.sort((first, second) => {
+                return second[1] - first[1];
+            })
+        } else {
+            fundsList.sort((first, second) => {
+                return first[1] - second[1];
+            })
+        }
+    }
 
     return (
         <>
@@ -30,11 +53,14 @@ const Funds = () => {
                     </div>
                     <div className='funds-grid'>
                         <h4 className='fund'>Fondo</h4>
-                        <h4 className='fund'>Cantidad</h4>
-                        {Object.keys(totalFunds.funds).map((fund, index) => (
+                        <h4 className='fund' onClick={() => changeFundsListOrder()}>
+                            Cantidad
+                            <FontAwesomeIcon icon={faArrowDown} className={descendingOrder ? 'arrow' : 'arrow rotated'}/>
+                            </h4>
+                        {fundsList.map((fund) => (
                             <React.Fragment key={fund}>
-                                <h4 className='fund'>{fund}</h4>
-                                <h4 className='fund'>{totalFunds.funds[fund]}</h4>
+                                <h4 className='fund'>{fund[0]}</h4>
+                                <h4 className='fund'>{fund[1]}</h4>
                             </React.Fragment>
                         ))}
                     </div>
